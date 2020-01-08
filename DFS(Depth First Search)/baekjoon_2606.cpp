234e3ed27ce27038bbdfca9369014cebe2dc1e92
sanggi <sangiki07@gmail.com>
2020-01-09 00:09:48 +0900

    바이러스(2606)

--- DFS(Depth First Search)/baekjoon_2606.cpp
+++ DFS(Depth First Search)/baekjoon_2606.cpp
@@ -5,24 +5,32 @@ using namespace std;
 
 // 바이러스(2606)
 
-void DFS(int start, vector<vector<int>> graph, bool visited[], int count){
+int DFS(int start, vector<vector<int>> graph, bool visited[], int count){
     visited[start] = true;
     count ++;
     for(int i=0; i<graph[start].size(); i++){
 
         if(graph[start][i] == 1 && visited[i] == false){
             cout << start << " -> " << i << endl;
-            cout <<" 감염: " << count << endl;            
-            DFS(i, graph, visited, count);
+            cout <<" 감염: " << count << endl;
+            count = DFS(i, graph, visited, count);
         }
     }
-    
+    return count;
 }
 
-void init(bool visited[], vector< vector<int> >graph, int comSize){
+vector< vector<int> > init(bool visited[], vector< vector<int> >graph, int comSize){
         fill(visited, visited+comSize, false);
 
+        for(int i=0; i<comSize; i++){
+            vector<int> linkedV(comSize);
+            graph.push_back(linkedV);
+            for(int j=0; j<comSize; j++){
+                graph[i].push_back(0);
+            }        
 
+        }
+        return graph;
 }
 int main()
 {
@@ -35,41 +43,27 @@ int main()
     // 방문여부
     bool visited[comSize];
     vector< vector<int> > graph(comSize);
-    init(visited, graph, comSize);
+    graph = init(visited, graph, comSize);
 
 
-    for(int i=0; i<comSize; i++){
-        vector<int> linkedV(comSize);
-        graph.push_back(linkedV);
-
-    }
-    // cout << "!!!! :" <<  endl;
-    for(int i=0; i<comSize; i++){
-        for(int j=0; j<comSize; j++){
-            graph[i].push_back(0);
-        }
-        
-    }
-
-    for(int i=0; i<comSize; i++){
-        cout << "vertex :" << i << endl;
-        // cout << "graph :" << graph[i][0] << endl;
-        for(int j=0; j<comSize; j++){
-            cout << graph[i][j] << " ";
-        }
-        cout << endl;
-    }    
 
     for(int i=0; i<edgeSize; i++){
         cin >> fromEdge >> toEdge;
-        // cout << "value :" << fromEdge << toEdge << endl;
         graph[fromEdge][toEdge] = 1;
         graph[toEdge][fromEdge] = 1;
 
     }
+    
 
+    for(int i=0; i<comSize; i++){
+        for(int j=0; j<comSize; j++){
+             cout << graph[i][j] << " " ;
+        }
+        cout << endl;
+    }
 
     cin >> start;
+    
     DFS(start, graph, visited, 0);
     
     return 0;
