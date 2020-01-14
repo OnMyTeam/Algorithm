04c8da4544a90c1de7a8b590c9c562440673ad24
sanggi <sangiki07@gmail.com>
2020-01-15 00:13:39 +0900

    BackTracking 기본예제

--- src/backtracking_example.cpp
+++ src/backtracking_example.cpp
@@ -7,7 +7,7 @@ int cnt = 0;
 int *cols;
 bool possible(int level){
     for(int i=0; i < level; i++){
-        if(cols[i] == cols[level] || abs(cols[i] - cols[level]) == (i - level)){
+        if(cols[i] == cols[level] || abs(cols[level] - cols[i]) == (level - i)){
             return false;
         }
     }
@@ -22,9 +22,11 @@ void queens(int level){
     for(int i=0; i < N; i++){
         cols[level] = i;
         if(possible(level))
+
             queens(level + 1);
         
     }
+
 }
 
 int main(){
@@ -33,7 +35,6 @@ int main(){
 
     for(int i=0; i<N; i++){
         cols[i] = 0;
-        cout << cols[i] << endl;
     }
     
     queens(0);
