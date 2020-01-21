890ef2f9b2f00171361531f00053d916059a1afd
MyTeamMyTeam <sangiki07@gmail.com>
2020-01-21 15:45:49 +0900

    01타일(1904)

--- src/baekjoon_1904.cpp
+++ src/baekjoon_1904.cpp
@@ -4,22 +4,33 @@
 using namespace std;
 
 int sum = 0;
-
+int *arrValue;
 int fibonacci(int n){
 	if(n == 1){
-
+		arrValue[n] = 1;
+		
 		return 1;
 	}
 	if(n == 2){
-
+		arrValue[n] = 2;
 		return 2;
 	}
-	sum = fibonacci(n - 1) + fibonacci(n -2);
+	if(arrValue[n] !=0){
+		return arrValue[n];
+	}
+	sum = fibonacci(n - 1) + fibonacci(n -2) ;
+	arrValue[n] = sum % 15746;
 	return sum;
 }
 int main(){
-	int N = 4;
-	int result = fibonacci(N);
+	int N = 1000000;
+	arrValue = new int[N + 1];
+
+	for(int i=0; i<=N; i++){
+		arrValue[i] = 0;
+	}
+	
+	int result = fibonacci(N % 15746);
 	cout << result << endl;
 	
 
