64f8b3ea9b04cd9d42c59465de8f28f35d0bf09d
sanggi <sangiki07@gmail.com>
2020-01-22 23:01:35 +0900

    피보나치수2(2748)

--- src/baekjoon_2748.cpp
+++ src/baekjoon_2748.cpp
@@ -4,20 +4,30 @@
 using namespace std;
 
 int sum = 0;
-
+int *arrValue;
 int fibonacci(int n){
 	if(n == 0){
+		arrValue[n] = 0;
 		return 0;
 	}
 	if(n == 1){
+		arrValue[n] = 1;
 		return 1;
 	}
-	sum = fibonacci(n - 1) + fibonacci(n -2);
-	return sum;
+	if(arrValue[n] != 0){
+		return arrValue[n];
+	}
+	sum = fibonacci(n - 1) + fibonacci(n - 2);
+	arrValue[n] = sum;
+	return arrValue[n];
 }
 int main(){
 	
-	int N = 10;
+	int N = 50;	
+	arrValue = new int[N + 1];
+	for(int i=0; i<=N; i++){
+		arrValue[i] = 0;
+	}
 	int result = fibonacci(N);
 	cout << result << endl;
 
