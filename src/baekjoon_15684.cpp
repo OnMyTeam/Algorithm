41ae6f0f91c6ce011a1d9d1ea29a285a6ac5b5ed
sanggi <sangiki07@gmail.com>
2020-02-19 23:16:02 +0900

    사다리조작(15684)

--- src/baekjoon_15684.cpp
+++ src/baekjoon_15684.cpp
@@ -9,11 +9,11 @@ const int M = 5;
 const int H = 6;
 int stickCnt;
 int ladder[H][N];
+
 bool success;
 void runLadderDFS(int row, int cnt){
 
 	if(stickCnt == cnt){
-		cout << "start!! " << endl;
 		success == true;
 		for(int i=0; i < N; i++){
 			
@@ -21,33 +21,28 @@ void runLadderDFS(int row, int cnt){
 			for(int j=0; j < H; j++){
 
 				if(ladder[j][r] == 1){
-					cout << j << " ," << r << endl;
+					// cout << j << " ," << r << endl;
 					r = r + 1;
-					cout << j << " ," << r << endl;
+					// cout << j << " ," << r << endl;
 				}
 				else if(r > 0 && ladder[j][r - 1] == 1){
-					cout << j << " ," << r << endl;
+					// cout << j << " ," << r << endl;
 					r = r - 1;
-					cout << j << " ," << r << endl;
+					// cout << j << " ," << r << endl;
 
 				}else{
-					cout << j << " ," << r << endl;
+					// cout << j << " ," << r << endl;
 				}
 				
 			}
-			if(i == r){
-				cout << "correct!! " << r << endl;
+			if(i != r){
+				success = false;
+				// cout << "fault!! " << r << endl;
+				break;
 				
 			}
-			else{
-				success = false;
-				cout << "fault!! " << r << endl;
-				break;
-
-			}
-			cout  << endl;
 		}
-
+		// cout << "success!! " << success << endl;
 		if(success == true){
 			cout << "success!! " << endl;
 			return;
@@ -57,10 +52,9 @@ void runLadderDFS(int row, int cnt){
 	for(int i = row; i < H; i++){
 		for(int j=0; j < N; j++){
 			if(ladder[i][j] != 1 && ladder[i][j - 1] != 1 && ladder[i][j + 1] != 1){
-				ladder[i][j] == 1;
-				cout << i << " ,,," << j << endl;
+				ladder[i][j] = 1;
 				runLadderDFS(i, cnt + 1);
-				ladder[i][j] == 0;
+				ladder[i][j] = 0;
 			}
 
 		}
@@ -69,8 +63,8 @@ void runLadderDFS(int row, int cnt){
 }
 
 int main(void){
-	for(int i=0; i < N; i++){
-		for(int j=0; j < H; j++){
+	for(int i=0; i < H; i++){
+		for(int j=0; j < N; j++){
 			ladder[i][j] = 0;
 		}
 	}
@@ -79,7 +73,13 @@ int main(void){
 	ladder[1][2] = 1;
 	ladder[4][0] = 1;
 	ladder[4][3] = 1;
-
+	for(int z=0; z < H; z++){
+		for(int x=0; x < N; x++){
+			cout << ladder[z][x] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 	stickCnt = 3;
 	runLadderDFS(0, 0);
 
