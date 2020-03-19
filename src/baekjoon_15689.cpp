d5012decdaba0f21ef9e7b1a37c1e6ecabd3d683
MyTeamMyTeam <sangiki07@gmail.com>
2020-03-19 16:28:39 +0900

    감시(15689)

--- src/baekjoon_15689.cpp
+++ src/baekjoon_15689.cpp
@@ -8,14 +8,25 @@ const int M = 6;
 
 int minNUm = 999999999;
 void run(int x, int y, int cameranum, int cValue[N][M]) {
-
+    
+    if(cameranum == 0){
+        for(int j=0; j<N; j++){
+            for(int k=0; k<M; k++){
+                if(0 < cValue[j][k] && cValue[j][k] < 6){
+                    int cameranum = cValue[j][k];
+                    
+                    run(j, k, cameranum, cValue);
+                }
+            }
+        }
+    }
     if(cameranum == 1){
         for(int i = 1; i <= 4; i++){
             // 동
             if(i == 1){
                 for(int j= y + 1; j < M; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }
                     cValue[x][j] = 9;
                 }
@@ -33,8 +44,8 @@ void run(int x, int y, int cameranum, int cValue[N][M]) {
 
             }else if(i == 2){ // 북
                 for(int j= x - 1; j >= 0; j--){
-                    if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                    
                     cValue[j][y] = 9;
                 }
@@ -52,7 +63,7 @@ void run(int x, int y, int cameranum, int cValue[N][M]) {
             }else if(i == 3){ // 서
                 for(int j= y - 1; j >= 0; j--){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                        
                     cValue[x][j] = 9;
                 }
@@ -70,7 +81,7 @@ void run(int x, int y, int cameranum, int cValue[N][M]) {
             }else if(i == 4){
                 for(int j= x + 1; j < N; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                       
                     cValue[x][j] = 9;
                 }
@@ -91,59 +102,61 @@ void run(int x, int y, int cameranum, int cValue[N][M]) {
         for(int i = 1; i <= 2 ; i++){
 
             if(i == 1){
+                int aValue[N][M] = cValue[N][M];
                 // 동
                 for(int j= y + 1; j < M; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                    
                     cValue[x][j] = 9;
                 }
                 // 서
                 for(int j= y - 1; j >= 0; j--){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                       
                     cValue[x][j] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
+                for(int i=0; i < N; i++){
+                    for(int j=0; j < M; j++){
+
+                        cout << cValue[i][j] << " ";
                     }
-                }                                          
+                    cout << endl;
+
+                }                
+                cout << endl;
+                                                   
             }else {
                 //북
                 for(int j= x - 1; j >= 0; j--){
-                    if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                       
                     cValue[j][y] = 9;
                 }
                 // 남
                 for(int j= x + 1; j < N; j++){
-                    if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                      
-                    cValue[x][j] = 9;
+                    cValue[j][y] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }                                         
 
             }
+            for(int j= x; j<N; j++){
+                for(int k = 0; k<M; k++){
+                    if(j == x && k == y){
+                        // cout<< "찾음" << cValue[j][k] << endl;
+                        // cout<< "위치" << j << "," << k << endl;
+                        continue;
+                    }
+                    if(0 < cValue[j][k] && cValue[j][k] < 6){
+                        int cameranum = cValue[j][k];
+                        run(j, k, cameranum, cValue);
+                    }
+                }
+            }            
         }
     }else if(cameranum == 3){
         for(int i = 1; i <= 4; i++){
@@ -152,101 +165,76 @@ void run(int x, int y, int cameranum, int cValue[N][M]) {
                 //동
                 for(int j= y + 1; j < M; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }
                     cValue[x][j] = 9;
                 }
                 for(int j= x - 1; j >= 0; j--){
-                    if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                    
                     cValue[j][y] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }                
+              
 
             }else if(i == 2){ // 북
                 for(int j= x - 1; j >= 0; j--){
-                    if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                    
                     cValue[j][y] = 9;
                 }
                 for(int j= y - 1; j >= 0; j--){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                        
                     cValue[x][j] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }                             
+                            
             }else if(i == 3){ // 서
                 for(int j= y - 1; j >= 0; j--){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                        
                     cValue[x][j] = 9;
                 }
+                // 남
                 for(int j= x + 1; j < N; j++){
-                    if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
-                    }                       
-                    cValue[x][j] = 9;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
+                    }                      
+                    cValue[j][y] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }                               
+                              
             }else if(i == 4){
+                // 남
                 for(int j= x + 1; j < N; j++){
-                    if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
-                    }                       
-                    cValue[x][j] = 9;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
+                    }                      
+                    cValue[j][y] = 9;
                 }
                 for(int j= y + 1; j < M; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }
                     cValue[x][j] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }                              
+                            
             }
+            for(int j= x; j<N; j++){
+                for(int k = 0; k<M; k++){
+                    if(j == x && k == y){
+                        // cout<< "찾음" << cValue[j][k] << endl;
+                        // cout<< "위치" << j << "," << k << endl;
+                        continue;
+                    }
+                    if(0 < cValue[j][k] && cValue[j][k] < 6){
+                        int cameranum = cValue[j][k];
+                        run(j, k, cameranum, cValue);
+                    }
+                }
+            }                  
         }  
 
     }else if(cameranum == 4){
@@ -256,14 +244,14 @@ void run(int x, int y, int cameranum, int cValue[N][M]) {
                 //동
                 for(int j= y + 1; j < M; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }
                     cValue[x][j] = 9;
                 }
                 //북
                 for(int j= x - 1; j >= 0; j--){
-                    if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                    
                     cValue[j][y] = 9;
                 }
@@ -271,151 +259,125 @@ void run(int x, int y, int cameranum, int cValue[N][M]) {
                 
                 for(int j= y - 1; j >= 0; j--){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                       
                     cValue[x][j] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }                                     
+                                    
 
             }else if(i == 2){ 
                 // 북
                 for(int j= x - 1; j >= 0; j--){
-                    if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                    
                     cValue[j][y] = 9;
                 }
                 // 서
                 for(int j= y - 1; j >= 0; j--){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                        
                     cValue[x][j] = 9;
                 }
+
                 // 남
                 for(int j= x + 1; j < N; j++){
-                    if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
                     }                      
-                    cValue[x][j] = 9;
+                    cValue[j][y] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }                                          
+                                            
             }else if(i == 3){ 
 
                 // 서
                 for(int j= y - 1; j >= 0; j--){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                        
                     cValue[x][j] = 9;
                 }
                 // 남
                 for(int j= x + 1; j < N; j++){
-                    if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
-                    }                       
-                    cValue[x][j] = 9;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
+                    }                      
+                    cValue[j][y] = 9;
                 }
                 //동
                 for(int j= y + 1; j < M; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }
                     cValue[x][j] = 9;
                 }
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }            
+           
 
             }else if(i == 4){
                 // 남
                 for(int j= x + 1; j < N; j++){
-                    if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
-                    }                       
-                    cValue[x][j] = 9;
+                    if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                        break;
+                    }                      
+                    cValue[j][y] = 9;
                 }
                 // 동
                 for(int j= y + 1; j < M; j++){
                     if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }
                     cValue[x][j] = 9;
                 }
                 // 북
                 for(int j= x - 1; j >= 0; j--){
                     if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                        continue;
+                        break;
                     }                    
                     cValue[j][y] = 9;
                 }                            
-                for(int j= x; j<N; j++){
-                    for(int k = 0; k<M; k++){
-                        if(cValue[j][k] == cValue[x][y]){
-                            continue;
-                        }
-                        if(0 < cValue[j][k] && cValue[j][k] < 6){
-                            int cameranum = cValue[j][k];
-                            run(j, k, cameranum, cValue);
-                        }
-                    }
-                }               
+             
             }
+            for(int j= x; j<N; j++){
+                for(int k = 0; k<M; k++){
+                    if(j == x && k == y){
+                        // cout<< "찾음" << cValue[j][k] << endl;
+                        // cout<< "위치" << j << "," << k << endl;
+                        continue;
+                    }
+                    if(0 < cValue[j][k] && cValue[j][k] < 6){
+                        int cameranum = cValue[j][k];
+                        run(j, k, cameranum, cValue);
+                    }
+                }
+            }                
         }  
     }else {
         // 남
         for(int j= x + 1; j < N; j++){
-            if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                continue;
-            }                       
-            cValue[x][j] = 9;
+            if(cValue[j][y] == 9 || cValue[j][y] == 6){
+                break;
+            }                      
+            cValue[j][y] = 9;
         }
         // 동
         for(int j= y + 1; j < M; j++){
             if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                continue;
+                break;
             }
             cValue[x][j] = 9;
         }
         // 북
         for(int j= x - 1; j >= 0; j--){
             if(cValue[j][y] == 9 || cValue[x][j] == 6){
-                continue;
+                break;
             }                    
             cValue[j][y] = 9;
         }
         // 서
         for(int j= y - 1; j >= 0; j--){
             if(cValue[x][j] == 9 || cValue[x][j] == 6){
-                continue;
+                break;
             }                        
             cValue[x][j] = 9;
         }
@@ -454,14 +416,7 @@ int main()
     };
 
 
-    for(int j=0; j<N; j++){
-        for(int k=0; k<M; k++){
-            if(0 < value[j][k] && value[j][k] < 6){
-                int cameranum = value[j][k];
-                run(j, k, cameranum, value);
-            }
-        }
-    }
+    run(0, 0, 0, value);
     
     return 0;
 }
