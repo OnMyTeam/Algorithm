39b98fa4c8b3756401228d71db22dec76000847a
OnMyTeam <sangiki07@gmail.com>
2021-04-07 19:48:12 +0900

    number-of-island(200)

--- Python/number-of-islands.py
+++ Python/number-of-islands.py
@@ -2,17 +2,37 @@
 
 
 ## 내풀이
-def my_solution(s: []) -> None:
-    pass
+def DFS(grid, i, j):
+    if i < 0 or i >= len(grid) or \
+        j < 0 or j >= len(grid[0]) or \
+            grid[i][j] != '1':
+        return
+    grid[i][j] = '0'
+
+    DFS(grid, i, j + 1)
+    DFS(grid, i + 1, j)
+    DFS(grid, i, j - 1)
+    DFS(grid, i - 1, j)
 
 
 
+def my_solution(grid = [[]]) -> int:
+    island_count = 0;
 
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                DFS(grid, i, j)
+
+
+                island_count += 1
+
+    return island_count
 if __name__ == '__main__':
     grid = [
-        ["1", "1", "1", "1", "0"],
-        ["1", "1", "0", "1", "0"],
         ["1", "1", "0", "0", "0"],
-        ["0", "0", "0", "0", "0"]
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]
     ]
-    solution1(["H", "a", "n", "n", "a", "h"])
+    print(my_solution(grid))
