4e8117f3d2726cbf5b935a97114f7a7e58287e71
OnMyTeam <sangiki07@gmail.com>
2021-04-09 16:16:58 +0900

    permutations(46)

--- Python/permutations(46).py
+++ Python/permutations(46).py
@@ -2,14 +2,13 @@
 
 
 ## 내풀이
-
 def my_solution(nums: []) -> []:
     result_list = []
 
-    def DFS(result, index):
+    def DFS(result):
 
         if len(result) == len(nums):
-            result_list.append(result)
+            result_list.append(result[:])
 
             return
 
@@ -17,17 +16,36 @@ def my_solution(nums: []) -> []:
             if num1 in result:
                 continue
             result.append(num1)
-            DFS(result, index + 1)
-            result = result[:index]
+            DFS(result)
+            result.pop()
 
     index = 0
     for num in nums:
         result = []
         result.append(num)
-        DFS(result, index + 1)
+        DFS(result)
 
     return result_list
+
+def solution1(nums: []) -> []:
+    results = []
+    prev_elements = []
+
+    def DFS(elements):
+
+        if len(elements) == 0:
+            results.append(prev_elements[:])
+            return
+        for e in elements:
+            next_elements = elements[:]
+            next_elements.remove(e)
+            prev_elements.append(e)
+            DFS(next_elements)
+            prev_elements.pop()
+    DFS(nums)
+    return results
+
 if __name__ == '__main__':
 
 
-    print(my_solution([1, 2, 3, 4]))
+    print(my_solution([1, 2, 3]))
