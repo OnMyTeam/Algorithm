77c4df6de0fe6ab5773fce0276f91c0ca24625d5
OnMyTeam <sangiki07@gmail.com>
2021-04-10 18:09:45 +0900

    combinations(77)

--- Python/combinations(77).py
+++ Python/combinations(77).py
@@ -8,13 +8,13 @@ def my_solution(n: int, k: int) -> []:
     result = []
     prev_elements = []
 
-    def DFS(index):
+    def DFS(start_num):
 
         if len(prev_elements) == k:
             result.append(prev_elements[:])
             return
 
-        for e in range(index, n):
+        for e in range(start_num, n):
             element = e + 1
             prev_elements.append(element)
             DFS(e + 1)
