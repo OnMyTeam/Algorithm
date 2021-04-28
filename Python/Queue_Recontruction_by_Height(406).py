f701cdc226a80c65e0e30d4c3ec2bd3d471edfc2
OnMyTeam <sangiki07@gmail.com>
2021-04-28 16:09:32 +0900

    Queue_Recontruction_by_Height(406)

--- Python/Queue_Recontruction_by_Height(406).py
+++ Python/Queue_Recontruction_by_Height(406).py
@@ -6,8 +6,7 @@ import heapq
 ## 내풀이
 def my_solution(people: List[List[int]]) -> List[List[int]]:
 
-    people.sort(key=itemgetter(0), reverse=True)
-    # print(people)
+    people.sort(key=lambda x:(-x[0],x[1]))
     result = []
     for v in people:
         result.insert(v[1], v)
@@ -19,13 +18,14 @@ def solution1(people: List[List[int]]) -> List[List[int]]:
     result = []
     for person in people:
         heapq.heappush(heap, (-person[0], person[1]))
-    print(heap)
+
     while heap:
         person = heapq.heappop(heap)
+
         result.insert(person[1], [-person[0], person[1]])
     return result
 if __name__ == '__main__':
     # people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
-    people = [[9,0],[7,0],[1,9],[3,0],[2,7],[5,3],[6,0],[3,4],[6,2],[5,2]]
-    print(solution1(people))
+    people = [[9,0],[7,0],[1,9],[3,0],[2,7],[6,0],[3,4],[6,2],[5,2],[5,3]]
+    # print(solution1(people))
     print(my_solution(people))
