8896995c055827c300b6b24a956a7441ebfdf7f5
OnMyTeam <sangiki07@gmail.com>
2021-05-03 17:58:50 +0900

    House_Robber(198)

--- Python/House_Robber(198).py
+++ Python/House_Robber(198).py
@@ -13,21 +13,25 @@ def my_solution(nums: List[int]) -> int:
     memorize = []
     memorize.append(nums[0])
     memorize.append(nums[1])
-
+    memorize.append(nums[0] + nums[2])
+    max_num = max(memorize)
     for i in range(len(nums)):
-        if i < 2:
+
+        if i < 3:
             max_num = max(max_num, nums[i])
         else:
             sum = nums[i] + memorize[i - 2]
+            sum1 = nums[i] + memorize[i - 3]
 
-            memorize.append(sum)
-            max_num = max(max_num, sum)
+            sub_max_num = max(sum, sum1)
+            memorize.append(sub_max_num)
 
+            max_num = max(max_num, sub_max_num)
 
     return max_num
 
 
 
 if __name__ == '__main__':
-    n = [1, 3, 1]
+    n = [1, 2, 3, 1]
     print(my_solution(n))
