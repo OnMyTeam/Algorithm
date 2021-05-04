ce8bb6f2309e3c7b4d3c26529b7b77d746bfa154
OnMyTeam <sangiki07@gmail.com>
2021-05-04 13:37:43 +0900

    reorderlogfiles(937)

--- Python/reorderlogfiles(937).py
+++ Python/reorderlogfiles(937).py
@@ -1,7 +1,8 @@
 ## 로그파일재정렬
-
+from typing import *
+import operator
 ## 내풀이
-def my_solution(logs: []) -> []:
+def my_solution(logs: List[str]) -> List[str]:
     digit_list = []
     letter_list = []
     for log in logs:
@@ -30,6 +31,53 @@ def my_solution(logs: []) -> []:
 
     return letter_list + digit_list
 
+def my_solution1(logs: []) -> []:
+    digit_list = []
+    letter_list = []
+    for log in logs:
+        identifier = "".join(log.split(" ")[1::])
+        if identifier.isdigit():
+            digit_list.append(log)
+        else:
+            letter_list.append(log)
+
+
+
+    for i, v in enumerate(letter_list):
+
+
+        for i1 in range(i + 1, len(letter_list)):
+            main_identifier = letter_list[i].split(" ")[0]
+            main_content = letter_list[i].split(" ")[1::]
+
+            identifier = letter_list[i1].split(" ")[0]
+            content = letter_list[i1].split(" ")[1::]
+
+            if (content == main_content and identifier < main_identifier) or (content < main_content):
+                temp_letter = letter_list[i1]
+                letter_list[i1] = letter_list[i]
+                letter_list[i] = temp_letter
+
+    return letter_list + digit_list
+
+
+def my_solution2(logs: List[str]) -> List[str]:
+    digits = []
+    letters = []
+
+    for l in logs:
+        if l.split()[1].isdigit():
+            digits.append(l)
+        else:
+            letters.append([l.split()[0], l.split()[1::]])
+
+    letters = sorted(letters, key=operator.itemgetter(1,0))
+    re_letters = []
+    for let in letters:
+        re_letters.append(let[0]+ " " + " ".join(let[1]))
+
+    return re_letters + digits
+
 ## 람다와 + 연산자 이용
 def solution1(logs: []) -> []:
     letters, digits = [], []
@@ -44,7 +92,7 @@ def solution1(logs: []) -> []:
     return letters + digits
 if __name__ == '__main__':
 
-    print(solution1(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]))
+    print(my_solution2(["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo","a7 act zoo"]))
     # ["27 85717 7", "2 y xyr fc", "52 314 99", "d 046099 0", "m azv x f", "7e apw c y", "8 hyyq z p", "6 3272401",
     #  "c otdk cl", "8 ksif m u"]
 
