1e742fff93d2c6008bd37b0f6e1a02c3b43ca9e0
sanggi <sangiki07@gmail.com>
2021-05-05 19:16:45 +0900

    Longest_Substring_Without_Repearing_Characters(3)

--- Python/Longest_Substring_Without_Repearing_Characters(3).py
+++ Python/Longest_Substring_Without_Repearing_Characters(3).py
@@ -1,5 +1,6 @@
 ## 중복문자없는 가장 긴 부분 문자열
 
+from typing import *
 
 import re, operator, collections
 
@@ -34,22 +35,53 @@ def my_solution(s: str) -> int:
 
     return max_length
 
+def my_solution2(s: str) -> int:
+    if s == "":
+        return 0
+    used_char = {}
+    max_length = 1
+    temp_length = 1
+    index = 0
+
+    str = s[0]
+    used_char[str] = 0
+    for i in range(1, len(s)):
+        if s[i] not in str:
+            str += s[i]
+            max_length = max(max_length, temp_length)
+
+
+        else:
+
+            max_length = max(max_length, temp_length)
+            temp_length = 1
+            str = s[i]
+        used_char[s[i]] = index
+    return max_length
+
 def solution1(s: str) -> int:
     max_length = start_index = 0
     used_char = {}
+
     for index, char in enumerate(s):
 
         if char in used_char and start_index <= used_char[char]:
 
             start_index = used_char[char] + 1
+
+            print(index, start_index, used_char[char], char)
+
         else:
             max_length = max(max_length, index - start_index + 1)
 
         used_char[char] = index
+
     return max_length
 if __name__ == '__main__':
     s = "abcabcbb"
     s = "aabaab!bb"
-    # s = "dvdf"
-    print(my_solution(s))
+    s = "au"
+    s = "aabaab!bb"
+
+    print(solution1(s))
 
